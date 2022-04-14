5d1abfa67cf0622b5a9a001ca7a12c1cf552fef4
Budiman Fajar Firdaus <budimanfajarf@gmail.com>
2022-04-15 00:14:46 +0700

    Add base fetch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
 import 'App.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
 // import BuiltInHeader from 'components/BuiltInHeader';
 
 function App() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+
+  // Note: the empty deps array [] means
+  // this useEffect will run once
+  // similar to componentDidMount()
+  useEffect(() => {
+    fetch("https://api.example.com/items")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setItems(result);
+          setIsLoaded(true);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setError(error);
+          setIsLoaded(true);
+        }
+      );
+  }, []);
+
   return (
     <div className="App">
       {/* <BuiltInHeader /> */}
+
       <main className="App-main">
-        It's blank
+        {error ? (
+          <div>Error: {error.message}</div>
+        ) : !isLoaded ? (
+          <div>Loading...</div>
+        ) : items.length === 0 ? (
+          <div>No items found.</div>
+        ) : (
+          <div>
+            {items}
+          </div>
+        )}
       </main>
     </div>
   );
